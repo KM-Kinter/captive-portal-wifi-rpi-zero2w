25e46722f1d77897baf6cf0dc1e96fca09e84650
KM-Kinter <kirilmedvedev00@gmail.com>
2024-12-29 14:34:28 +0100

    Add web interface for connect WiFi networks

--- wifi_checkup.py
+++ wifi_checkup.py
@@ -1,46 +1,42 @@
-import time
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import subprocess
-from flask import Flask, render_template_string
-import wifi
 
 app = Flask(__name__)
 
-def check_internet():
-    try:
-        subprocess.check_call(['ping', '-c', '1', 'google.com'])
+def is_connected():
+    wlan0_ip = subprocess.getoutput("ip addr show wlan0 | grep inet")
+    eth0_ip = subprocess.getoutput("ip addr show eth0 | grep inet")
+    
+    if wlan0_ip or eth0_ip:
         return True
-    except subprocess.CalledProcessError:
+    else:
         return False
 
-def get_available_networks():
-    networks = wifi.Cell.all('wlan0')
-    return networks
+def connect_to_wifi(ssid, password):
+    config = f"""
+    network={{
+        ssid="{ssid}"
+        psk="{password}"
+    }}
+    """
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as file:
+        file.write(config)
+    
+    subprocess.run(["sudo", "wpa_cli", "-i", "wlan0", "reconnect"])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # if check_internet():
-    #     return "Jesteś połączony z internetem!", 200
-    # else:
-        networks = get_available_networks()
-        return render_template_string("""
-            <html>
-            <head><title>Wybierz sieć Wi-Fi</title></head>
-            <body>
-                <h1>Brak połączenia z internetem</h1>
-                <p>Wybierz jedną z dostępnych sieci Wi-Fi:</p>
-                <ul>
-                    {% for network in networks %}
-                        <li>{{ network.ssid }}</li>
-                    {% endfor %}
-                </ul>
-            </body>
-            </html>
-        """, networks=networks)
+    if is_connected():
+        return render_template('status.html', status="Connected to a network!")
+    else:
+        if request.method == 'POST':
+            ssid = request.form['ssid']
+            password = request.form['password']
+            if ssid and password:
+                connect_to_wifi(ssid, password)
+                return redirect(url_for('index'))
+        return render_template('index.html')
 
-def start_flask_server():
-    # Uruchomienie serwera na porcie 80
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
-if __name__ == "__main__":
-    start_flask_server()
