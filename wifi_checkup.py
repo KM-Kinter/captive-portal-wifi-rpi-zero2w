c7ddde8a8e2f8e8f36e0bc3fbb37e202bc0e5bfa
KM-Kinter <kirilmedvedev00@gmail.com>
2024-12-29 14:41:32 +0100

    Bypass connect status for testing webui

--- wifi_checkup.py
+++ wifi_checkup.py
@@ -1,42 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
 import subprocess
+from flask import Flask, render_template, request
+import wifi
 
 app = Flask(__name__)
 
-def is_connected():
-    wlan0_ip = subprocess.getoutput("ip addr show wlan0 | grep inet")
-    eth0_ip = subprocess.getoutput("ip addr show eth0 | grep inet")
-    
-    # if wlan0_ip or eth0_ip:
-    #     return True
-    # else:
-    return False
+def get_available_networks():
+    networks = wifi.Cell.all('wlan0')
+    return networks
 
-def connect_to_wifi(ssid, password):
-    config = f"""
-    network={{
-        ssid="{ssid}"
-        psk="{password}"
-    }}
-    """
-    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as file:
-        file.write(config)
-    
-    subprocess.run(["sudo", "wpa_cli", "-i", "wlan0", "reconnect"])
+def connect_to_network(network_name, password):
+    cmd = f"sudo nmcli dev wifi connect '{network_name}' password '{password}'"
+    subprocess.run(cmd, shell=True)
+
+def is_connected():
+    wifi_connected = subprocess.run(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    eth_connected = subprocess.run(['ip', 'link', 'show', 'eth0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return wifi_connected.returncode == 0 or eth_connected.returncode == 0
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
     if is_connected():
         return render_template('status.html', status="Connected to a network!")
-    else:
-        if request.method == 'POST':
-            ssid = request.form['ssid']
-            password = request.form['password']
-            if ssid and password:
-                connect_to_wifi(ssid, password)
-                return redirect(url_for('index'))
-        return render_template('index.html')
+    
+    if request.method == 'POST':
+        network_name = request.form['network']
+        password = request.form['password']
+        connect_to_network(network_name, password)
+        return render_template('status.html', status=f"Connecting to {network_name}...")
+
+    networks = get_available_networks()
+    return render_template('index.html', networks=networks)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
