d5b1523d28dc49e5dd786ab644f7a17648065fc7
KM-Kinter <kirilmedvedev00@gmail.com>
2024-12-29 14:44:31 +0100

    Bypass connect status for testing webui

--- wifi_checkup.py
+++ wifi_checkup.py
@@ -12,15 +12,15 @@ def connect_to_network(network_name, password):
     cmd = f"sudo nmcli dev wifi connect '{network_name}' password '{password}'"
     subprocess.run(cmd, shell=True)
 
-def is_connected():
-    wifi_connected = subprocess.run(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    eth_connected = subprocess.run(['ip', 'link', 'show', 'eth0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return wifi_connected.returncode == 0 or eth_connected.returncode == 0
+# def is_connected():
+#     wifi_connected = subprocess.run(['iwgetid'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     eth_connected = subprocess.run(['ip', 'link', 'show', 'eth0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     return wifi_connected.returncode == 0 or eth_connected.returncode == 0
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if is_connected():
-        return render_template('status.html', status="Connected to a network!")
+    # if is_connected():
+    #     return render_template('status.html', status="Connected to a network!")
     
     if request.method == 'POST':
         network_name = request.form['network']
